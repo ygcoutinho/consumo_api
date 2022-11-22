06b8777853f70f494e8e98187270a3e5040c8407
Yago Coutinho <yagogc.it@gmail.com>
2022-11-21 23:22:50 -0300

    serealização json da classe curso

--- lib/models/curso.dart
+++ lib/models/curso.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class Curso {
   final int id;
   final String nome;
@@ -8,4 +10,24 @@ class Curso {
     required this.nome,
     required this.isAluno,
   });
+
+  //transforma um objeto da classe em um map
+  Map<String, dynamic> toMap() {
+    return {"id": id, "nome": nome, "isAluno": isAluno};
+  }
+
+  //cria um objeto da classe a partir de um map recebido por parâmetro
+  factory Curso.fromMap(Map<String, dynamic> map) {
+    return Curso(
+      id: map["id"] ?? 0,
+      nome: map["nome"] ?? "",
+      isAluno: map["isAluno"] ?? false,
+    );
+  }
+
+  //converte o map do objeto para uma string json
+  String toJson() => jsonEncode(toMap());
+
+  //cria um objeto da classe a partir de um json que será convertido em map
+  factory Curso.fromJson(String json) => Curso.fromMap(jsonDecode(json));
 }
