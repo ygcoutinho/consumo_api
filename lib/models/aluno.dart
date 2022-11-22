0852d4fcb4a1a2eec6cb8202447165e93038f1d0
Yago Coutinho <yagogc.it@gmail.com>
2022-11-21 23:51:20 -0300

    serealização classe aluno

--- lib/models/aluno.dart
+++ lib/models/aluno.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:consumo_api/models/curso.dart';
 import 'package:consumo_api/models/endereco.dart';
 
@@ -17,4 +19,30 @@ class Aluno {
     required this.endereco,
     required this.cursos,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      "id": id,
+      "nome": nome,
+      "idade": idade,
+      "nomeCursos": nomeCursos,
+      "endereco": endereco.toMap(),
+      "cursos": cursos.map((curso) => curso.toMap()).toList(),
+    };
+  }
+
+  factory Aluno.fromMap(Map<String, dynamic> map) {
+    return Aluno(
+      id: map["id"],
+      nome: map["nome"],
+      idade: map["idade"],
+      nomeCursos: map["nomeCursos"],
+      endereco: Endereco.fromMap(map["endereco"]),
+      cursos: map["cursos"].map<Curso>((cursoMap) => Curso.fromMap(cursoMap)).toList(),
+    );
+  }
+
+  String toJson() => jsonEncode(toMap());
+
+  factory Aluno.fromJson(String json) => Aluno.fromMap(jsonDecode(json));
 }
